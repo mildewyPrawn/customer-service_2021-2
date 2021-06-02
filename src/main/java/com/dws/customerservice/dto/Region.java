6cbf2b27d077bdd4fcb5c0ac628d871047ebe510
Emiliano <galeanaara@ciencias.unam.mx>
2021-06-02 16:30:47 -0500

    feature CRUD Region implementation
    Se realizó toda la implementación del CRUD Region.
    
    Responsable: Emiliano Galeana
    API: Customer
    Sprint 1

--- src/main/java/com/dws/customerservice/dto/Region.java
+++ src/main/java/com/dws/customerservice/dto/Region.java
@@ -5,8 +5,26 @@ public class Region {
 	private int id;
 	private String region;
 	
+	public Region () {}
+	
 	public Region(int id, String region) {
 		this.id = id;
+		this.region = region;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
 		this.region = region;
 	}
 }
