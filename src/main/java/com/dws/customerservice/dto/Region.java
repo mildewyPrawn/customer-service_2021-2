64bc2281e5f3b9089763c51026492a59904ccae5
Emiliano <galeanaara@ciencias.unam.mx>
2021-06-02 00:17:54 -0500

    feature CRUD_Region add dto region
    Se creó el dto de region
    
    Funcionalidad: CRUD_Region
    API: Customer
    Responsable: Emiliano Galeana Araujo

---
+++ src/main/java/com/dws/customerservice/dto/Region.java
@@ -0,0 +1,12 @@
+package com.dws.customerservice.dto;
+
+public class Region {
+
+	private int id;
+	private String region;
+	
+	public Region(int id, String region) {
+		this.id = id;
+		this.region = region;
+	}
+}
